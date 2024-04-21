ede8661e3f164946061fc8eb8b0becbce709354d
임병욱 <limbyeongwook@imbyeong-ug-ui-MacBookPro.local>
2024-04-22 01:37:45 +0900

    feat: screen 설정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,32 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react';
+import RootNavigator from './src/navigations/root/RootNavigator';
 
 function App(): JSX.Element {
-  const [name, setName] = useState('');
-
-  const handleChangeInput = (text: string) => {
-    console.log(text);
-    setName(text);
-  };
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.inputContainer}>
-          <Text>이름</Text>
-          <TextInput
-            style={styles.input}
-            value={name}
-            onChangeText={handleChangeInput}
-          />
-        </View>
-      </SafeAreaView>
+      <RootNavigator />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { backgroundColor: 'red' },
-});
 
 export default App;
